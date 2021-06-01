5187f23c8e39c6a70245c290529ca321ad79ffd8
Colm Talbot <colm.talbot@ligo.org>
2021-06-01 15:57:44 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+import numpy as np
+
+extensions = [
+    Extension("bilby_cython.geometry", ["bilby_cython/geometry.pyx"], include_dirs=[np.get_include()]),
+    Extension("bilby_cython.time", ["bilby_cython/time.pyx"], include_dirs=[np.get_include()]),
+]
+setup(
+    name="bilby.cython",
+    ext_modules=cythonize(extensions, language_level="3"),
+    packages=["bilby_cython"],
+)
