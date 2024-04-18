c148547abce8fe079c108f6e141e3b1e98499991
Colm Talbot <talbotcolm@gmail.com>
2024-04-18 16:46:54 -0500

    MAINT: remove old conftest

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import bilby_cython
-import pytest
-
-
-@pytest.fixture(params=bilby_cython.SUPPORTED_BACKENDS)
-def backend(request):
-    bilby_cython.set_backend(request.param)
-    return request.param
-
