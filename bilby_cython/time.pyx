95d8d228de5d3859664b9c7e7854bc442a203de7
Colm Talbot <colm.talbot@ligo.org>
2024-08-21 18:59:16 +0000

    BUG: fix definition of leap seconds

--- bilby_cython/time.pyx
+++ bilby_cython/time.pyx
@@ -5,7 +5,7 @@ cimport cython
 
 cdef datetime GPS_EPOCH = datetime(1980, 1, 6, 0, 0, 0)
 cdef double EPOCH_J2000_0_JD = 2451545.0
-cdef long[:] LEAP_SECONDS = [
+cdef long[18] LEAP_SECONDS = [
     46828800,
     78364801,
     109900802,
