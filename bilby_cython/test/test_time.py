b26cdf6502ba38666b0217d2650e782c9f9be714
Colm Talbot <talbotcolm@gmail.com>
2024-08-22 18:54:17 +0000

    TST: reduce test stringency

--- bilby_cython/test/test_time.py
+++ bilby_cython/test/test_time.py
@@ -54,7 +54,7 @@ def test_gmt(types):
             greenwich_sidereal_time(tt, eq)
             - lal.GreenwichSiderealTime(tt, eq)
         )
-    assert max(np.abs(diffs)) < 1e-10
+    assert max(np.abs(diffs)) < 1e-5
 
 
 def test_current_time():
